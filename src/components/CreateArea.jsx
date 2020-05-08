10deed74ae3ee826253dd687b7af71d61e518c32
Saimadhav <Saimadhav-git>
2020-05-08 19:26:17 +0530

    Keeper-local

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  const [state, setstate] = useState(false);
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+  function handletextarea() {
+    setstate(true);
+  }
+  return (
+    <div>
+      <form className="create-note">
+        {state ? (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        ) : null}
+        <textarea
+          onClick={handletextarea}
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={state ? 3 : 1}
+        />
+        {state ? (
+          <Zoom in={state}>
+            <Fab onClick={submitNote}>
+              <AddIcon />
+            </Fab>
+          </Zoom>
+        ) : null}
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
